5db8a7eb0af1363632b54611154d63d4004d09bb
revus05 <skninni05@gmail.com>
2025-06-24 06:07:44 +0300

    chore: complete project set up

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+import './globals.css'
+
+const roboto = Roboto({
+  variable: '--font-roboto',
+  subsets: ['latin', 'cyrillic'],
+})
+
+export const metadata: Metadata = {
+  title: 'Skriney',
+  description: 'App to track expenses',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} antialiased`}>{children}</body>
+    </html>
+  )
+}
