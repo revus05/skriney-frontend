fed3f32fa127c9386da483939b995a43fe545604
revus05 <skninni05@gmail.com>
2025-07-31 21:21:04 +0300

    chore(api-generation): set up openapi-typescript-codegen to generate types from backend models

---
+++ src/shared/api/api-client/index.ts
@@ -0,0 +1,7 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type { LoginRequestDTO } from './models/LoginRequestDTO';
+export type { RegisterRequestDTO } from './models/RegisterRequestDTO';
---
+++ src/shared/api/api-client/models/RegisterRequestDTO.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type RegisterRequestDTO = {
+    /**
+     * Username
+     */
+    username: string;
+    /**
+     * Email address
+     */
+    email: string;
+    /**
+     * User password
+     */
+    password: string;
+};
+
