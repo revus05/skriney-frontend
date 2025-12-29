ef67eb6cbdd333789de3bab1181a46b8e9f9d948
revus <skninni05@gmail.com>
2025-12-29 07:08:59 +0300

    feat: update default bank account after delete

--- src/entities/bank-account/model/useDeleteBankAccount.ts
+++ src/entities/bank-account/model/useDeleteBankAccount.ts
@@ -10,13 +10,21 @@ export const useDeleteBankAccount = () => {
     (state) => state.authSlice.user?.userSettings,
   )
   const dispatch = useAppDispatch()
+  const bankAccounts = useAppSelector(
+    (state) => state.bankAccountsSlice.bankAccounts,
+  )
 
   return async (data: { uuid: string }) => {
     try {
       dispatch(deleteBankAccount(data.uuid))
       if (userSettings?.defaultBankAccount?.uuid === data.uuid) {
         dispatch(
-          setUserSettings({ ...userSettings, defaultBankAccount: undefined }),
+          setUserSettings({
+            ...userSettings,
+            defaultBankAccount: bankAccounts.filter(
+              (bankAccount) => bankAccount.uuid !== data.uuid,
+            )[0],
+          }),
         )
       }
       deleteBankAccountFn(data)
