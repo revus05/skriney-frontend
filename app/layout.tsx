7c27c32b9a4b7fe579e0142e4b8e9bce9d0f942e
revus <skninni05@gmail.com>
2026-01-02 12:09:23 +0300

    feat: enhance background gradient && make connect telegram button text semibold

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,11 @@ const RootLayout = async ({
                 'animate-fly-circle absolute top-1/4 left-[30vw] h-[50svh] w-[50vw] -translate-1/2 rotate-[-33deg] rounded-[50%] bg-[#CC9766] opacity-100 blur-[25vw] motion-reduce:animate-none'
               }
             />
+            <div
+              className={
+                'animate-gradient-pulse absolute right-0 -bottom-1/4 h-[50svh] w-[20vw] rotate-[30deg] rounded-[50%] bg-[#19ad64] opacity-30 blur-[25vw] motion-reduce:animate-none'
+              }
+            />
           </div>
         </div>
       </body>
